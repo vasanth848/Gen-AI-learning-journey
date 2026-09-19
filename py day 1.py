8f2ac404c9b8948dd828d29255d5b1fb43275351
Vasanthan S <vasanthan4624@gmail.com>
2026-09-19 21:52:25 +0530

    Add files via upload

---
+++ py day 1.py
@@ -0,0 +1,21 @@
+#print statement:
+v=10
+print(v)
+
+V=20
+print(v+V)
+
+#type inseption:
+print (type(v))
+print(type(V))
+
+#type conversion:
+s=float(v)
+print(s)
+
+#magical operator: 
+print(v.__add__(V))
+
+
+
+
