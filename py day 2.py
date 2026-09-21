8801c39f3a884eb1829ce89d493cf3f1834d24cc
Vasanthan S <vasanthan4624@gmail.com>
2026-09-21 13:38:34 +0530

    Add files via upload

---
+++ py day 2.py
@@ -0,0 +1,34 @@
+#variable names
+#variable name is case sensitive
+value=20
+Value=30
+print(Value)
+
+#data types
+#int
+a=10
+#float
+b=2.6
+#string
+c="str"
+#boolean
+d=(a<b)
+print(d)
+print(type(a))
+
+#string basics
+string="str"
+print(string[2])
+print(string[0])
+print(string[:3])
+string='stringbasics'
+print(string[0:])
+print(string[3:7])
+
+
+
+
+
+
+
+
