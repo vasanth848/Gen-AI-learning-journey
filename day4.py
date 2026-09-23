6f4763f76533c2ce773db5f4ec2d029cbf327bcf
Vasanthan S <vasanthan4624@gmail.com>
2026-09-23 23:08:13 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,17 @@
+#string handling
+#concatenation
+f_name="kobe"
+l_name="bryant"
+number=24
+full_name=(f_name + l_name)
+print(f_name + l_name)
+
+
+#we can change data types for our need 
+print(f_name + str(number))
+
+#repeting string
+print(full_name*5)
+full_name1="kobe bryant "
+print(full_name1*5)
+print((full_name1.split(" "))*5)
